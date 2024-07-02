8e25b80ede21d3a9752d9337ff9de3a00cc765ea
Rebeca Carvalho <149064661+rebecarv@users.noreply.github.com>
2024-07-02 15:41:22 -0300

    Add files via upload

---
+++ filter/helpers.c
@@ -0,0 +1,125 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Take average of red, green, and blue
+            float average =
+                (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
+            int n = round(average);
+
+            // Update pixel values
+            image[i][j].rgbtRed = n;
+            image[i][j].rgbtGreen = n;
+            image[i][j].rgbtBlue = n;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Compute sepia values
+            // The resulting red, green, and blue values will be whole numbers between 0 and 255,
+            // inclusive.
+            int sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen +
+                                 .189 * image[i][j].rgbtBlue);
+            int sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen +
+                                   .168 * image[i][j].rgbtBlue);
+            int sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen +
+                                  .131 * image[i][j].rgbtBlue);
+
+            // Update pixel with sepia values
+            image[i][j].rgbtRed = fmin(255, sepiaRed);
+            image[i][j].rgbtGreen = fmin(255, sepiaGreen);
+            image[i][j].rgbtBlue = fmin(255, sepiaBlue);
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        // Loop over half of the pixels in the row
+        for (int j = 0, n = width / 2; j < n; j++)
+        {
+            // Swap pixels
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
+    return;
+}
+
+// Blur image
+
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Create a copy of image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int totalRed = 0, totalBlue = 0, totalGreen = 0;
+            float counter = 0.00;
+
+            // Get neighbouring pixels
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    int currentX = i + x;
+                    int currentY = j + y;
+
+                    if (currentX < 0 || currentX > (height - 1) || currentY < 0 ||
+                        currentY > (width - 1))
+                    {
+                        continue;
+                    }
+                    totalRed += image[currentX][currentY].rgbtRed;
+                    totalGreen += image[currentX][currentY].rgbtGreen;
+                    totalBlue += image[currentX][currentY].rgbtBlue;
+
+                    counter++;
+                }
+            }
+            copy[i][j].rgbtRed = round(totalRed / counter);
+            copy[i][j].rgbtGreen = round(totalGreen / counter);
+            copy[i][j].rgbtBlue = round(totalBlue / counter);
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = copy[i][j];
+        }
+    }
+    return;
+}
